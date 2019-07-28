ed36179b14cd9ce15b71cfa84080900b933ccaba
Sergey19940808 <aleksey.serzh@mail.ru>
2019-07-28 15:38:42 +0500

    Update app.js

--- src/shortener/static/shortener/app.js
+++ src/shortener/static/shortener/app.js
@@ -1,17 +1,27 @@
 'use strict';
-const links = document.querySelectorAll('.link_table');
 
-links.forEach(link=> {
+const short_links = document.querySelectorAll('.link_table');
+const origin_links = document.querySelectorAll('.link_table-origin');
 
-    link.addEventListener('click', (event) => handlerCount(event));
-    link.removeEventListener('click', (event) => handlerCount(event));
+short_links.forEach(link=> {
+    link.addEventListener('click', (event) => handlerShortLink(event));
+    link.removeEventListener('click', (event) => handlerShortLink(event));
+});
+origin_links.forEach(link=> {
+    link.addEventListener('click', (event) => handlerOriginLink(event));
+    link.removeEventListener('click', (event) => handlerOriginLink(event));
 });
 
-const handlerCount = (event) => {
+const handlerShortLink = (event) => {
   event.preventDefault();
   const idShortener = parseInt(event.target.dataset.id);
   fetch('/shortener/count/'+idShortener).then((response)=> {
       location.reload();
       window.open(event.target.href, '_blank');
   })
+};
+
+const handlerOriginLink = (event) => {
+  event.preventDefault();
+  window.open(event.target.href, '_blank');
 };
