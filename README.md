22d03b827fd78ae57fd457507cd34278baafb17a
Sergey <aleksey.serzh@mail.ru>
2019-08-02 15:56:55 +0500

    remove all prjs

--- README.md
+++
@@ -1,3 +0,0 @@
-# shortener
-Прежде должны быть установлены docker, docker-compose.
-Запуск командой: docker-compose up 
--- src/shortener/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from shortener.views import ShortenerView, CountShortenerView
-
-urlpatterns = [
-    path('', ShortenerView.as_view(), name='index'),
-    path('count/<int:pk>', CountShortenerView.as_view(), name='count')
-]
